16138494016582bcbd5978a719ea719c234df5d9
Ibrahim Atef <ebrahemafroto653@gmail.com>
2024-04-08 15:19:19 +0200

    add di add phone number regex add routes for register add register screen and its components

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -2,6 +2,8 @@ import 'package:complete_project/core/networking/api_service.dart';
 import 'package:complete_project/core/networking/dio_factory.dart';
 import 'package:complete_project/features/login/data/repos/login_repo.dart';
 import 'package:complete_project/features/login/logic/cubit/login_cubit.dart';
+import 'package:complete_project/features/register/data/repos/register_repo.dart';
+import 'package:complete_project/features/register/logic/cubit/register_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -14,5 +16,9 @@ Future<void> setupGetIt() async {
 
   // login
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
-  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
+
+  //register 
+  getIt.registerLazySingleton<RegisterRepo>(() => RegisterRepo(getIt()));
+  getIt.registerFactory<RegisterCubit>(() => RegisterCubit(getIt()));
 }
